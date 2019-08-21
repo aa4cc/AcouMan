c05184b9f4d5b294155a1615919682791574b6d5
Josef Matouš <matoujo3@fel.cvut.cz>
2019-08-21 12:09:02 +0200

    working controller for manipulating triangular objects

--- simulink/triangle_startup.m
+++ simulink/triangle_startup.m
@@ -1,5 +1,3 @@
 %% Add folders to path
-addpath('system_object/bfgs','system_object/newGenerator','controller_constants/triangle','MATLAB_scripts');
-%% Load constants
-load('correction.mat');
-load('H_unrot.mat');
+addpath('system_object/bfgs','system_object/newGenerator','MATLAB_scripts', ...
+    'system_object/loader','../raspi-ballpos/matlabSystemObject');
